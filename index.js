c8fe69287c0c7fa4a37c25939bcf5e185b7c7336
likejosiane <josi13.b@gmail.com>
2022-09-24 17:33:59 -0500

    Cambios

--- index.js
+++ index.js
@@ -8,9 +8,68 @@ function Calculate(){
     }
     catch(err)
     {
-        alert('ERROR DE CÁLCULO')
+        alert('ERROR DE CÁLCULO');
     }
 }
 function Clear(){
     resultado.value = "";
+} 
+
+function sin(){
+    resultado.value=Math.sin(resultado.value);
+} 
+
+function cos(){
+    resultado.value=Math.cos(resultado.value);
+} 
+
+function tag(){
+    resultado.value=Math.tan(resultado.value);
+} 
+function elevadoTres(){
+    resultado.value=Math.pow(resultado.value, 3);
+}
+
+function raizCuadrada(){
+    resultado.value=Math.sqrt(resultado.value,2);
+} 
+ 
+function elevadoN(){
+    resultado.value = Math.pow(resultado.value, )
+}
+
+function raizCubica(){
+    resultado.value = Math.pow(resultado.value, 1/3);
+}
+
+function log(){
+    resultado.value=Math.log(resultado.value);
+} 
+
+function pi(){
+    resultado.value=3.14159265359;
+} 
+
+function e(){
+    resultado.value=2.71828182846;
+}
+
+function fact(){
+    let i, num, f;
+    f=1
+    num=resultado.value;
+    for(i=1; i<=num; i++){
+        f=f*i;
+    }
+    i=i-1;
+    resultado.value=f;
+
+}
+
+function backspc(){
+    resultado.value=resultado.value.substr(0,resultado.value.length-1);
+}
+
+function e(){
+    resultado.value=2.71828182846;
 }
